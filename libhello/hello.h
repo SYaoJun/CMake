a366f06adc30d848ea52aae3842893985bc97844
yaojun <940334249@qq.com>
2022-11-27 16:19:04 +0800

    update cmake lectures

---
+++ libhello/hello.h
@@ -0,0 +1,12 @@
+//
+// Created by 姚军 on 2022/11/27.
+//
+
+#ifndef CMAKE_STUDY_HELLO_H
+#define CMAKE_STUDY_HELLO_H
+
+
+void hello();
+
+
+#endif //CMAKE_STUDY_HELLO_H
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "add.h"
+#include <iostream>
+
+int main() {
+    int a = add(999, 199);
+    std::cout<<a<<std::endl;
+    return 0;
+}
