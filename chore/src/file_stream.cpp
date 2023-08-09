2cef648bc21fa64c1ca104b2ab1923460b5e9fe0
yaojun <940334249@qq.com>
2023-08-09 10:51:54 +0800

    fix: practice fstream

---
+++ chore/src/file_stream.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+int main()
+{
+
+    std::fstream my_file_;// 实例化一个文件对象
+    std::string data;
+    my_file_.open("firstFile.txt", std::ios_base::in | std::ios_base::out | std::ios::app); // 打开文件firstFile.txt，可选择三种模式
+    if (my_file_.is_open())                                                         // 检测open()是否成功
+    {
+
+        my_file_.write(data.data(), data.size());
+        my_file_<<"Writing in file";
+        my_file_.close(); // 关闭文件流以保存其内容，这一步不能忘
+    }
+}
