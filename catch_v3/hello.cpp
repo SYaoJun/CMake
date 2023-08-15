95ae1c57da7d6b778c83fce2bae8dc1933ed55ae
yaojun <940334249@qq.com>
2023-08-15 09:52:30 +0800

    fix: catch2 include path

--- catch_v3/hello.cpp
+++ catch_v3/hello.cpp
@@ -1,18 +1,21 @@
-#include <catch2/catch_test_macros.hpp>
+#include "catch2/catch_test_macros.hpp"
 
-static int Factorial( int number ) {
-return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
+static int Factorial(int number)
+{
+    return number <= 1 ? 1 : Factorial(number - 1) * number; // pass
 }
 
-TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
-    REQUIRE( Factorial(0) == 1 );
+TEST_CASE("Factorial of 0 is 1 (fail)", "[single-file]")
+{
+    REQUIRE(Factorial(0) == 1);
 }
 
-TEST_CASE( "Factorials of 1 and higher are computed (pass)", "[single-file]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE("Factorials of 1 and higher are computed (pass)", "[single-file]")
+{
+    REQUIRE(Factorial(1) == 1);
+    REQUIRE(Factorial(2) == 2);
+    REQUIRE(Factorial(3) == 6);
+    REQUIRE(Factorial(10) == 3628800);
 }
 
 // Compile & run:
