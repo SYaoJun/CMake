d3846337b90d5237e0b8f908b7b85f97dc2629ca
yaojun <940334249@qq.com>
2023-08-15 11:35:38 +0800

    add: abseil_test

---
+++ abseil/abseil_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "absl/strings/str_join.h"
+
+int main()
+{
+    std::vector<std::string> v = {"foo", "bar", "baz"};
+    std::string s = absl::StrJoin(v, "-");
+
+    std::cout << "Joined string: " << s << "\n";
+}
